c3affdd230d993bfa45057841764a6d7fd8101d0
Your Name <catticus.bayne@proton.me>
2025-08-29 11:12:46 -0700

    Fixed spike bugs

--- Assets/Scripts/PlayerStats.cs
+++ Assets/Scripts/PlayerStats.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections;
+using System.Runtime.CompilerServices;
 using TarodevController;
 using UnityEngine;
 
@@ -9,19 +10,39 @@ public class PlayerStats : MonoBehaviour
     private int currentHealth;
     [SerializeField] private GameObject spawnRoom;
     [SerializeField] private Vector3 spawnPoint;
+    [SerializeField] GameObject healthBar;
+    GameObject heart0;
+    GameObject heart1;
+    GameObject heart2;
+    GameObject heart3;
+    GameObject heart4;
     private float iFramesSeconds = 1;
 
     private void Awake()
     {
         currentHealth = startingHealth;
-        // switch (SaveFile.GetSavePoint())
-        // {
-        //     case 0:
-        //         spawnRoom = (GameObject)Resources.Load("Levels/Room0");
-        //         spawnPoint = new Vector3(-5, 5, 0);
-        //         break;
-        //     default: throw new NotImplementedException();
-        // }
+#if UNITY_STANDALONE
+        switch (SaveFile.GetSavePoint())
+        {
+            case 0:
+                spawnRoom = (GameObject)Resources.Load("Levels/Room0");
+                spawnPoint = new Vector3(-5, 5, 0);
+                break;
+            default: throw new NotImplementedException();
+        }
+#endif
+    }
+    private void Start()
+    {
+        heart0 = healthBar.transform.GetChild(0).gameObject;
+        heart1 = healthBar.transform.GetChild(1).gameObject;
+        heart2 = healthBar.transform.GetChild(2).gameObject;
+        heart3 = healthBar.transform.GetChild(3).gameObject;
+        heart4 = healthBar.transform.GetChild(4).gameObject;
+    }
+    private void Update()
+    {
+        SetHealthBar(); 
     }
     public void TakeDamage(int damage)
     {
@@ -52,6 +73,49 @@ public class PlayerStats : MonoBehaviour
         Physics2D.IgnoreLayerCollision(6, 9, true);
         yield return new WaitForSeconds(iFramesSeconds);
         Physics2D.IgnoreLayerCollision(6, 9, false);
+    }
+    private void SetHealthBar()
+    {
+        if (currentHealth == 1)
+        {
+            heart0.SetActive(true);
+            heart1.SetActive(false);
+            heart2.SetActive(false);
+            heart3.SetActive(false);
+            heart4.SetActive(false);
+        }
+        if (currentHealth == 2)
+        {
+            heart0.SetActive(true);
+            heart1.SetActive(true);
+            heart2.SetActive(false);
+            heart3.SetActive(false);
+            heart4.SetActive(false);
+        }
+        if (currentHealth == 3)
+        {
+            heart0.SetActive(true);
+            heart1.SetActive(true);
+            heart2.SetActive(true);
+            heart3.SetActive(false);
+            heart4.SetActive(false);
+        }
+        if (currentHealth == 4)
+        {
+            heart0.SetActive(true);
+            heart1.SetActive(true);
+            heart2.SetActive(true);
+            heart3.SetActive(true);
+            heart4.SetActive(false);
+        }
+        if (currentHealth >= 5)
+        {
+            heart0.SetActive(true);
+            heart1.SetActive(true);
+            heart2.SetActive(true);
+            heart3.SetActive(true);
+            heart4.SetActive(true);
+        }
     }
 
 
